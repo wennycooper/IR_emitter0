1b8592b749cbbd75974579eaa3cf8610f86a367a
wennycooper <kevin.kuei.0321@gmail.com>
2016-04-28 12:54:54 +0800

    ...

--- IR_emitter0.ino
+++ IR_emitter0.ino
@@ -1,5 +1,9 @@
-#include <IRremote.h>
+#include <CppList.h>
 
+//#include <IRremote.h>
+//#include <IRremoteInt.h>
+
+#include <IRremote.h>
 IRsend irsend; // IRRemote限定使用數位腳位3
 
 void setup()
@@ -7,43 +11,12 @@ void setup()
   Serial.begin(115200);
 }
 
+unsigned long v;
 void loop() {
-  int d;
-  if(true) { // 讀取序列埠
-    d = '1';
-    unsigned long v = 0x0;
+  v = 0x77E14050;
 
-    /*
-    switch(d){ // 根據讀取到的資料，換成代表六個按鍵的紅外線編碼
-      case '1':
-        v = 0x77E14050;
-      break;
-      case '2':
-        v = 0x77E12050;
-      break;
-      case '3':
-        v = 0x77E1D050;
-      break;
-      case '4':
-        v = 0x77E1B050;
-      break;
-      case '5':
-        v = 0x77E1E050;
-      break;
-      case '6':
-        v = 0x77E11050;
-      break;
-    }
-    */
-    
-    v = 0x77E14050;
-    if(v != 0x0){
-      //Serial.print("read ");
-      //Serial.print(d);
-      Serial.print(", IR send ");
-      Serial.println(v, HEX);
-      irsend.sendNEC(v, 32); // 輸出紅外線訊號
-      delay(31);
-    }
+  while (1) {
+    irsend.sendNEC(v, 32); // 輸出紅外線訊號
+    delay(31);
   }
 }
